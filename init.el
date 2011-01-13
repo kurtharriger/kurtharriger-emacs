ad36bb7f5b124f8317302e31363b25c18484e6ca
Kurt Harriger <kurtharriger@gmail.com>
2011-01-13 12:58:52 -0700

    add groovy mode

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
                                (scroll-up 1))))
 
 ;; Viper mode
-(setq viper-mode t)
+;;(setq viper-mode t)
 (require 'viper)
 
 
@@ -51,6 +51,21 @@
   (defun js2-custom-setup ()
     (moz-minor-mode 1))
 
+
+;; groovy-mode
+;;; turn on syntax highlighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; make Groovy mode electric by default.
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
 
 
 
