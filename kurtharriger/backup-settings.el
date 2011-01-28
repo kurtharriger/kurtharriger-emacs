3a525a1a05ce77eb6f9a0a3df23cfcc0e6ebb4f1
Kurt Harriger <kurtharriger@gmail.com>
2011-01-28 10:58:55 -0700

    add groovy mode, disable backup files

---
+++ kurtharriger/backup-settings.el
@@ -0,0 +1,6 @@
+;; disable backups and autosave to prevent clutering working directory
+(setq make-backup-files nil)
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+(setq auto-save-hash-p nil)
+
