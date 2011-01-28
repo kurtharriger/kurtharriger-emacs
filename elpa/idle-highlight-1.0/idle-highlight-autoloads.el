c2f5911ac3dc463a6c939dbf5c2c0654a6a6dddd
Kurt Harriger <kurtharriger@gmail.com>
2011-01-28 10:50:54 -0700

    install packages

---
+++ elpa/idle-highlight-1.0/idle-highlight-autoloads.el
@@ -0,0 +1,28 @@
+;;; idle-highlight-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (idle-highlight) "idle-highlight" "idle-highlight.el"
+;;;;;;  (19779 61))
+;;; Generated autoloads from idle-highlight.el
+
+(autoload 'idle-highlight "idle-highlight" "\
+Toggle idle-highlighting.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("idle-highlight-pkg.el") (19779 61 84886))
+
+;;;***
+
+(provide 'idle-highlight-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; idle-highlight-autoloads.el ends here
