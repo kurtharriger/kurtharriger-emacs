3acf490a4880a6bff47f93284352528b890b1170
Kurt Harriger <kurtharriger@gmail.com>
2011-01-28 10:11:52 -0700

    relative load path

--- kurtharriger.el
+++
@@ -1,8 +0,0 @@
-;; kurtharriger.el
-
-
-(let ((default-directory "~/.emacs.d/kurtharriger/"))
-  (normal-top-level-add-to-load-path '("."))
-  (normal-top-level-add-subdirs-to-load-path))
-
- 
--- kurtharriger/zencoding.el
+++ kurtharriger/zencoding.el
@@ -1,3 +1,4 @@
+(add-to-list 'load-path "zencoding")
 
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode)  
